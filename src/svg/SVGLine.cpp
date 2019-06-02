4dc7291d5e450e42a4df0c9eae85012a24e47201
Thibault Lejemble <thibault.le@live.fr>
2019-06-03 00:47:03 +0200

    Line

---
+++ src/svg/SVGLine.cpp
@@ -0,0 +1,86 @@
+#include <svg/SVGLine.h>
+#include <svg/SVGStyle.h>
+
+#include <ostream>
+
+namespace svg {
+
+SVGLine::SVGLine(const std::string& id,
+                 float x1,
+                 float y1,
+                 float x2,
+                 float y2) :
+    SVGShape(id),
+    m_x1(x1),
+    m_y1(y1),
+    m_x2(x2),
+    m_y2(y2)
+{
+}
+
+SVGLine::SVGLine(float x1,
+                 float y1,
+                 float x2,
+                 float y2,
+                 const std::string& id) :
+    SVGShape(id),
+    m_x1(x1),
+    m_y1(y1),
+    m_x2(x2),
+    m_y2(y2)
+{
+}
+
+float SVGLine::x1() const
+{
+    return m_x1;
+}
+
+float SVGLine::y1() const
+{
+    return m_y1;
+}
+
+float SVGLine::x2() const
+{
+    return m_x2;
+}
+
+float SVGLine::y2() const
+{
+    return m_y2;
+}
+
+void SVGLine::set_x1(float x1)
+{
+    m_x1 = x1;
+}
+
+void SVGLine::set_y1(float y1)
+{
+    m_y1 = y1;
+}
+
+void SVGLine::set_x2(float x2)
+{
+    m_x2 = x2;
+}
+
+void SVGLine::set_y2(float y2)
+{
+    m_y2 = y2;
+}
+
+void SVGLine::print(std::ostream &os) const
+{
+    os << "<line\n"
+       << "x1=\"" << m_x1 << "\"\n"
+       << "y1=\"" << m_y1 << "\"\n"
+       << "x2=\"" << m_x2 << "\"\n"
+       << "y2=\"" << m_y2 << "\"\n"
+       << "style=\"";
+    m_style->print(os);
+    os << "\"/>";
+}
+
+} // namespace svg
