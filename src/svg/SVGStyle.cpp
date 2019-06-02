c09658906023789f2226452a13b483c61689f8d7
Thibault Lejemble <thibault.le@live.fr>
2019-06-02 23:20:12 +0200

    wip Style

---
+++ src/svg/SVGStyle.cpp
@@ -0,0 +1,163 @@
+#include <svg/SVGStyle.h>
+
+namespace svg {
+
+SVGStyle::SVGStyle() :
+    m_opacity(1),
+    m_filled(true),
+    m_fill(0,0,0),
+    m_fill_opacity(1),
+    m_stroked(false),
+    m_stroke(0,0,0),
+    m_stroke_width(1),
+    m_stroke_linecap(Round),
+    m_stroke_miterlimit(4),
+    m_stroke_dashed(false),
+    m_stroke_dasharray({}),
+    m_stroke_dashoffset(0),
+    m_stroke_opacity(1),
+    m_paint_order(Normal)
+{
+}
+
+float SVGStyle::opacity() const
+{
+    return m_opacity;
+}
+
+void SVGStyle::set_opacity(float opacity)
+{
+    m_opacity = opacity;
+}
+
+bool SVGStyle::is_filled() const
+{
+    return m_filled;
+}
+
+void SVGStyle::set_filled(bool filled)
+{
+    m_filled = filled;
+}
+
+const SVGColor& SVGStyle::fill() const
+{
+    return m_fill;
+}
+
+void SVGStyle::set_fill(const SVGColor& fill)
+{
+    m_fill = fill;
+}
+
+float SVGStyle::fill_opacity() const
+{
+    return m_fill_opacity;
+}
+
+void SVGStyle::set_fill_opacity(float opacity)
+{
+    m_fill_opacity = opacity;
+}
+
+bool SVGStyle::is_stroked() const
+{
+    return m_stroked;
+}
+
+void SVGStyle::set_stroked(bool stroked)
+{
+    m_stroked = stroked;
+}
+
+const SVGColor& SVGStyle::stroke() const
+{
+    return m_stroke;
+}
+
+void SVGStyle::set_stroke(const SVGColor& stroke)
+{
+    m_stroke = stroke;
+}
+
+float SVGStyle::stroke_width() const
+{
+    return m_stroke_width;
+}
+
+void SVGStyle::set_stroke_width(float width)
+{
+    m_stroke_width = width;
+}
+
+Cap SVGStyle::stroke_linecap() const
+{
+    return m_stroke_linecap;
+}
+
+void SVGStyle::set_stroke_linecap(Cap linecap)
+{
+    m_stroke_linecap = linecap;
+}
+
+int SVGStyle::stroke_miterlimit() const
+{
+    return m_stroke_miterlimit;
+}
+
+void SVGStyle::set_stroke_miterlimit(int miterlimit)
+{
+    m_stroke_miterlimit = miterlimit;
+}
+
+bool SVGStyle::is_stroke_dashed() const
+{
+    return m_stroke_dashed;
+}
+
+void SVGStyle::set_stroke_dashed(bool dashed)
+{
+    m_stroke_dashed = dashed;
+}
+
+const std::vector<int>& SVGStyle::stroke_dasharray() const
+{
+    return m_stroke_dasharray;
+}
+
+void SVGStyle::set_stroke_dasharray(const std::vector<int>& dasharray)
+{
+    m_stroke_dasharray = dasharray;
+}
+
+int SVGStyle::stroke_dashoffset() const
+{
+    return m_stroke_dashoffset;
+}
+
+void SVGStyle::set_stroke_dashoffset(int dashoffset)
+{
+    m_stroke_dashoffset = dashoffset;
+}
+
+float SVGStyle::stroke_opacity() const
+{
+    return m_stroke_opacity;
+}
+
+void SVGStyle::set_stroke_opacity(float opacity)
+{
+    m_stroke_opacity = opacity;
+}
+
+Order SVGStyle::paint_order() const
+{
+    return m_paint_order;
+}
+
+void SVGStyle::set_paint_order(Order order)
+{
+    m_paint_order = order;
+}
+
+} // namespace svg
--- src/svg/SVGStyle.h
+++ src/svg/SVGStyle.h
@@ -2,30 +2,89 @@
 
 #include <svg/SVGColor.h>
 
+#include <vector>
+
 namespace svg {
+
+enum Cap : int
+{
+    Butt,
+    Round,
+    Square
+};
+
+enum Order : int
+{
+    Normal
+};
 
 class SVGStyle
 {
 public:
+    SVGStyle();
 
+public:
+    float opacity() const;
+    void set_opacity(float opacity);
 
+    bool is_filled() const;
+    void set_filled(bool filled = true);
+
+    const SVGColor& fill() const;
+    void set_fill(const SVGColor& fill);
+
+    float fill_opacity() const;
+    void set_fill_opacity(float opacity);
+
+    bool is_stroked() const;
+    void set_stroked(bool stroked = true);
+
+    const SVGColor& stroke() const;
+    void set_stroke(const SVGColor& stroke);
+
+    float stroke_width() const;
+    void set_stroke_width(float width);
+
+    Cap stroke_linecap() const;
+    void set_stroke_linecap(Cap linecap);
+
+    int stroke_miterlimit() const;
+    void set_stroke_miterlimit(int miterlimit);
+
+    bool is_stroke_dashed() const;
+    void set_stroke_dashed(bool dashed = true);
+
+    const std::vector<int>& stroke_dasharray() const;
+    void set_stroke_dasharray(const std::vector<int>& dasharray);
+
+    int stroke_dashoffset() const;
+    void set_stroke_dashoffset(int dashoffset);
+
+    float stroke_opacity() const;
+    void set_stroke_opacity(float opacity);
+
+    Order paint_order() const;
+    void set_paint_order(Order order);
 
 protected:
-    float       m_opacity;
+    float            m_opacity;
 
-    bool        m_filled;
-    SVGColor    m_fill;
-    float       m_fill_opacity;
+    bool             m_filled;
+    SVGColor         m_fill;
+    float            m_fill_opacity;
 
-    bool        m_stroked;
-    SVGColor    m_stroke;
-    float       stroke_width;
-//    stroke-linecap    ex: round
-    int stroke_miterlimit;
-//    stroke-dasharray  ex: none, (a,b)
-    int stroke_dashoffset;
-    float stroke_opacity;
-//    paint-order   ex: normal
+    bool             m_stroked;
+    SVGColor         m_stroke;
+    float            m_stroke_width;
+    Cap              m_stroke_linecap;
+    int              m_stroke_miterlimit;
+
+    bool             m_stroke_dashed;
+    std::vector<int> m_stroke_dasharray;
+    int              m_stroke_dashoffset;
+    float            m_stroke_opacity;
+
+    Order            m_paint_order;
 };
 
 } // namespace svg
