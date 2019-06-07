5fb8dedc35da5b3c2a96a4655acd9d4da3c80c4c
Thibault Lejemble <thibault.le@live.fr>
2019-06-07 23:08:27 +0200

    update example orientatiob

--- examples/example2_basic_shapes.cpp
+++ examples/example2_basic_shapes.cpp
@@ -5,12 +5,12 @@ using namespace svg;
 
 int main()
 {
-    SVGRectangle* rectangle = new SVGRectangle("rectangle", 10, 110, 80, 80);
-    SVGCircle*    circle    = new SVGCircle("circle", 150, 150, 40);
-    SVGEllipse*   ellipse   = new SVGEllipse("ellipse", 250, 150, 40, 20);
-    SVGLine*      line      = new SVGLine("line", 10, 10, 90, 90);
-    SVGPolyLine*  polyline  = new SVGPolyLine("polyline", {150,110,110,130,170,190,190,150}, {10,40,70,90,90,70,40,10});
-    SVGPolygon*   polygon   = new SVGPolygon("polygon", {250,210,210,230,270,290,290,250}, {10,40,70,90,90,70,40,10});
+    SVGRectangle* rectangle = new SVGRectangle("rectangle", 10, 10, 80, 80);
+    SVGCircle*    circle    = new SVGCircle("circle", 150, 50, 40);
+    SVGEllipse*   ellipse   = new SVGEllipse("ellipse", 250, 50, 40, 20);
+    SVGLine*      line      = new SVGLine("line", 10, 190, 90, 110);
+    SVGPolyLine*  polyline  = new SVGPolyLine("polyline", {150,110,110,130,170,190,190,150}, {190,160,130,110,110,130,160,190});
+    SVGPolygon*   polygon   = new SVGPolygon("polygon", {250,210,210,230,270,290,290,250}, {190,160,130,110,110,130,160,190});
 
     SVGDocument svg;
     svg.set_width(300);
