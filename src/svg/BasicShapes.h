afefd7a3f1357c3675e5c3c3ceced4c5d38a5b60
Thibault Lejemble <thibault.le@live.fr>
2019-06-07 23:02:39 +0200

    BasicShapes

---
+++ src/svg/BasicShapes.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <svg/SVGRectangle.h>
+#include <svg/SVGCircle.h>
+#include <svg/SVGEllipse.h>
+#include <svg/SVGLine.h>
+#include <svg/SVGPolyLine.h>
+#include <svg/SVGPolygon.h>
